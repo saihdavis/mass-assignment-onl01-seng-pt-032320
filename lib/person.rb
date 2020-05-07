1305216173dd79b4d7c6fa8ab0182d71adbe230d
Saida Davis <saihdavis@gmail.com>
2020-05-07 04:08:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,9 +3,5 @@ class Person
   attr_accessor :name, :birthday, :height, :weight
   
   def initialize(attributes)
-    @name = name
-    @birthday = birthday
-    @height = height
-    @weight = weight
-end
+   
 end
