5ad07d3169df06f60fb0414c6f1edc404ad9419b
Saida Davis <saihdavis@gmail.com>
2020-05-07 04:11:24 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,6 +3,6 @@ class Person
   attr_accessor :name, :birthday, :height, :weight
   
   def initialize(attributes)
-   attributes.each {|key, value| self.send(("#{key}=", value))}
+   attributes.each {|key, value| self.send(("#{key}="), value)}
 end
 end
